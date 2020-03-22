4a78f502c79bd2bceb965a439ad8b98dfc5d51d0
Arun Persaud <arun@nubati.net>
2020-03-21 17:22:04 -0700

    doko: first version

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -6,7 +6,7 @@ INTERP = "/home/arun/bin/python3.8"
 if sys.executable != INTERP:
     os.execl(INTERP, INTERP, *sys.argv)
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import time
 import random
 from pathlib import Path
@@ -118,7 +118,50 @@ def boggle_5x5():
 
 
 @application.route("/doko")
-def doko():
+@application.route("/doko", methods=["POST"])
+@application.route("/doko/<seed>")
+@application.route("/doko/<seed>/<player>")
+def doko(seed=None, player=None):
+
+    if player is not None:
+        tag = "{} {}".format(seed, player)
+        FILE = Path("tmp") / "doko.db"
+        if FILE.exists():
+            with FILE.open('r') as f:
+                for l in f:
+                    if l.startswith(tag):
+                        return render_template("doko-single-error.html")
+        random.seed(seed)
+        cards = list(range(48))
+        random.shuffle(cards)
+        if player == "A":
+            cards = cards[:12]
+        elif player == "B":
+            cards = cards[12:24]
+        elif player == "C":
+            cards = cards[24:36]
+        elif player == "D":
+            cards = cards[36:]
+        # normalize to just the odd numbers
+        cards = [2*(c//2)+1 for c in cards]
+        cards = sorted(cards)
+        cards = ["doko/{}.png".format(c) for c in cards]
+        with FILE.open('a') as f:
+            f.write("{}\n".format(tag))
+        return render_template("doko-game.html", cards=cards)
+
+    if request.method == "POST":
+        seed = request.form["name"].lower()
+        seed = seed.replace(" ", "")
+        out = ""
+        for s in seed:
+            if s.isalnum():
+                out += s
+        seed = out
+
+    if seed is not None:
+        return render_template("doko-start.html", seed=seed)
+
     return render_template("doko.html")
 
 
---
+++ static/doko/readme.txt
@@ -0,0 +1,8 @@
+These images are generated from the original SVGs via inkscape.
+
+The originals can be found at:
+   http://svg-cards.sourceforge.net/
+
+The SVG are released under the GNU LESSER GENERAL PUBLIC LICENSE
+
+Arun
