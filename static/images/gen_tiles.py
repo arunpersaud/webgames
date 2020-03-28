fd11902e61f0dcc99c12fce41036a5ba27fb57fe
Arun Persaud <arun@nubati.net>
2020-03-28 10:42:49 -0700

    boggle: small fixes for images

--- static/images/gen_tiles.py
+++ static/images/gen_tiles.py
@@ -9,49 +9,51 @@ import os
 
 
 def substitute(letter):
-  if letter == 'q':
-    return 'qu'
-  return letter
+    if letter == "Q":
+        return "Qu"
+    return letter
 
 
 def getParams(letter):
-  bone = "\#eaeaea"
-  brown = "\#753010"
-  dark = "\#332222"
-  cap = letter.capitalize()
-  params ={}
-  if (cap == 'Qu'):
-    params["background_color"] = brown
-    params["fill_color"] = bone
-  else:
-    params["background_color"] = dark
-    params["fill_color"] = bone
+    bone = "\#eaeaea"
+    brown = "\#753010"
+    dark = "\#332222"
+    params = {}
+    if letter == "Qu":
+        params["background_color"] = brown
+        params["fill_color"] = bone
+    else:
+        params["background_color"] = dark
+        params["fill_color"] = bone
 
-  params["letter"] = cap
-  params["point_size"] = 62
-  params["font"] = "helvetica-bold"
-  params["xdim"] = 100
-  params["ydim"] = 100
-  params["filename"] = "tile_{letter}.png".format(letter=letter)
+    params["letter"] = letter
+    params["point_size"] = 62
+    params["font"] = "helvetica-bold"
+    params["xdim"] = 100
+    params["ydim"] = 100
+    params["filename"] = f"tile_{letter}.png"
 
-  return params
+    return params
+
 
 def genCommand(params):
-  command_template = ("convert -background {background_color}"
-                      " -fill {fill_color} -font {font}"
-                      " -gravity center"
-                      " -border 4 -bordercolor darkgreen"
-                      " -pointsize {point_size}"
-                      " -size {xdim}x{ydim} label:{letter} {filename}")
-  return command_template.format(**params)
-                      
+    command_template = (
+        "convert -background {background_color}"
+        " -fill {fill_color} -font {font}"
+        " -gravity center"
+        " -border 4 -bordercolor darkgreen"
+        " -pointsize {point_size}"
+        " -size {xdim}x{ydim} label:{letter} {filename}"
+    )
+    return command_template.format(**params)
 
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 assert len(alphabet) == 26
 letters = [substitute(x) for x in alphabet]
 
 for letter in letters:
-  params = getParams(letter)
-  cmd = genCommand(params)
-  os.system(cmd)
-  print(cmd)
+    params = getParams(letter)
+    cmd = genCommand(params)
+    os.system(cmd)
+    print(cmd)
