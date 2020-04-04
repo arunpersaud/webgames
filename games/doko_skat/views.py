778054dfbe867e004545eb7935a34f57a352c3fb
Arun Persaud <arun@nubati.net>
2020-04-04 14:40:32 -0700

    use jinja include and extend, fix wording and base template, fix loading of css

--- games/doko_skat/views.py
+++ games/doko_skat/views.py
@@ -3,6 +3,7 @@ from ..util import nocache
 from pathlib import Path
 import time
 import random
+import datetime
 
 doko_skat = Blueprint(
     "doko_skat",
@@ -15,19 +16,25 @@ doko_skat = Blueprint(
 
 @doko_skat.route("/doko")
 @doko_skat.route("/doko", methods=["POST"])
-@doko_skat.route("/doko/<seed>")
-@doko_skat.route("/doko/<seed>/<player>")
-def doko(seed=None, player=None):
-
+@doko_skat.route("/doko/<seed>/<nr>/")
+@doko_skat.route("/doko/<seed>/<nr>/<player>")
+def doko(seed=None, player=None, nr=1):
+    nr = int(nr)
+    
     if player is not None:
-        tag = "{} {}".format(seed, player)
+        tag = "{} {} {}".format(seed, player, nr)
         FILE = Path("tmp") / "doko.db"
         if FILE.exists():
             with FILE.open("r") as f:
                 for l in f:
                     if l.startswith(tag):
                         return render_template("doko-single-error.html")
-        random.seed(seed)
+
+        now = datetime.datetime.now()
+        midnight = datetime.datetime.combine(now.date(), datetime.time())
+        seconds = (now - midnight).seconds
+
+        random.seed(seed+str(nr))
         cards = list(range(48))
         random.shuffle(cards)
         if player == "A":
@@ -44,7 +51,7 @@ def doko(seed=None, player=None):
         cards = ["doko/{}.png".format(c) for c in cards]
         with FILE.open("a") as f:
             f.write("{}\n".format(tag))
-        return render_template("doko-game.html", cards=cards)
+        return render_template("doko-game.html", cards=cards, nr=nr, seed=seed, player=player)
 
     if request.method == "POST":
         seed = request.form["name"].lower()
@@ -54,8 +61,18 @@ def doko(seed=None, player=None):
             if s.isalnum():
                 out += s
         seed = out
+        return redirect("/doko/{}/{}".format(seed, nr))
 
     if seed is not None:
-        return render_template("doko-start.html", seed=seed)
+        players = {'A': False, 'B': False, 'C': False, 'D': False}
+        for player in players:
+            tag = "{} {} {}".format(seed, player, nr)
+            FILE = Path("tmp") / "doko.db"
+            if FILE.exists():
+                with FILE.open("r") as f:
+                    for l in f:
+                        if l.startswith(tag):
+                            players[player] = True
+        return render_template("doko-start.html", seed=seed, nr=nr, players=players)
 
     return render_template("doko.html")
